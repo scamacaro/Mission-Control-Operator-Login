54e24b97f3090e23289e90d21401951535cfe34f
Sanyerlis Camacaro <105477237+scamacaro@users.noreply.github.com>
2022-06-04 00:11:58 -0600

    Add files via upload

---
+++ programming buttons.js
@@ -0,0 +1,11 @@
+//function for start button
+function start(){
+    document.getElementById("startButton").disabled = true;
+    document.getElementById("stopButton").disabled = false;
+    }
+    //function for stop button
+    function stop(){
+        document.getElementById("stopButton").disabled = true;
+        document.getElementById("startButton").disabled = false;
+    }
+    
